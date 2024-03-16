34b990065167734fe5793d6193edc812aa1c7242
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2024-03-16 19:37:00 -0400

    added page Ankit.js

---
+++ src/Ankit.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Ankit() {
+
+    return (
+       <center>
+        <h1> Welcome to Ankit's page</h1>
+        <a href="/">Back to Home</a>
+       </center>
+    );
+
+
+}
+export default Ankit;
